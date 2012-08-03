42ccf7fdc5a718f6dbbef0fe0ec6bdc563c8f273
Daniel Sundman <daniel@yds.se>
2012-08-03 23:24:56 +0200

    express 3.x.

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 
 var express = require('express'),
-	listRoutes = require('./routes/lists').routes;
+	listRoutes = require('./routes/lists').routes,
+	http = require('http'),
+	path = require('path');
 
-var app = module.exports = express.createServer();
+var app = module.exports = express();
 
 var port = process.env.PORT || 3000;
 
 // Configuration
 
-app.configure(function(){
+app.configure(function() {
+	app.set('port', port);
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'ejs');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(express.cookieParser());
-	app.use(express.session({ secret: 'your secret here' }));
+	app.use(express.session({ secret:'your secret here' }));
 	app.use(app.router);
-	app.use(express.static(__dirname + '/public'));
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function() {
+	app.use(express.errorHandler());
 });
 
 app.configure('production', function(){
@@ -31,8 +36,9 @@ listRoutes.forEach(function(r) {
 	app[r.verb](r.url, r.handler);
 })
 
-app.listen(port);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+http.createServer(app).listen(app.get('port'), function() {
+	console.log("Express server listening on port " + app.get('port'));
+});
 
 // curl -H "Accept: application/json" -H "Content-type: application/json" -X POST http://localhost:3000/lists/12 -d '{"x":3}'
 // curl -H "Accept: application/json" -H "Content-type: application/json" -X PUT http://localhost:3000/lists/12 -d '{"x":3}'
