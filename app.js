a70b4e110121db3d2c15024d92e4fbf1fad455f5
Daniel Sundman <daniel@yds.se>
2012-08-04 14:43:07 +0200

    JADE

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var port = process.env.PORT || 3000;
 app.configure(function() {
 	app.set('port', port);
 	app.set('views', __dirname + '/views');
-	app.set('view engine', 'ejs');
+	app.set('view engine', 'jade');
 	app.use(express.favicon());
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
@@ -34,7 +34,12 @@ app.configure('production', function(){
 
 listRoutes.forEach(function(r) {
 	app[r.verb](r.url, r.handler);
-})
+});
+
+app.get('/', function(req, res) {
+	console.log('/');
+	res.render('index');
+});
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log("Express server listening on port " + app.get('port'));
