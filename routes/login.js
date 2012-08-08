100d55215f3acab4fe1b25a5c269d6ba60f7fc60
Daniel Sundman <daniel@yds.se>
2012-08-08 18:50:28 +0200

    Fixed bug that didn't really care about whether authentication succeeded or not.

--- routes/login.js
+++ routes/login.js
@@ -5,22 +5,28 @@ function form(req, res) {
 	res.render('auth/login', { title: 'Login' });
 }
 
-function submit(req, res) {
+function invalidLogin(req, res) {
+	res.locals.error = 'Sorry! invalid credentials.';
+	form(req, res);
+}
+
+function submit(req, res, next) {
 	console.log('login.submit');
 	var data = req.body.user;
 	User.findOne({username:data.username}, function(err, user) {
-		console.log('user: ' + user);
 		if (err) return next(err);
 		if (user) {
 			user.authenticate(data.pwd, function(err, auth) {
-				console.log('authenticated!');
-				req.session.uid = user.id;
-				res.redirect('/');
-				return;
+				if (auth) {
+					req.session.uid = user.id;
+					res.redirect('/');
+				} else {
+					invalidLogin(req, res);
+				}
 			});
+		} else {
+			invalidLogin(req, res);
 		}
-		res.locals.error = 'Sorry! invalid credentials.';
-		form(req, res);
 	});
 }
 
